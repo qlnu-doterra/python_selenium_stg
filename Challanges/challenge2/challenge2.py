49c3adb874392f5fbede9068d079d9edf4207af6
qlnu-doterra <qlnu@doterra.com>
2019-09-15 13:35:54 -0600

    Challenge 3 completed.

--- Challanges/challenge2/challenge2.py
+++ Challanges/challenge2/challenge2.py
@@ -15,7 +15,7 @@ class Challenge2(unittest.TestCase):
     def tearDown(self) -> None:
         self.driver.close()
 
-    def test_challenge1(self):
+    def test_challenge2(self):
         # launch the site
         self.driver.get("https://www.copart.com")
         self.assertIn("Copart USA", self.driver.title)
@@ -34,11 +34,10 @@ class Challenge2(unittest.TestCase):
         list_of_car = list()
         table = WebDriverWait(self.driver, 10).until(
             EC.presence_of_element_located(
-                (By.XPATH, '//*[@id="serverSideDataTable"]/tbody/tr')))
-        tableRows = self.driver.find_elements_by_xpath('//*[@id="serverSideDataTable"]/tbody/tr')
+                (By.XPATH, '//*[@id="serverSideDataTable"]/tbody')))
+        tableRows = table.find_elements_by_xpath('.//tr')
         for i in range(len(tableRows)):
-            car_make = self.driver.find_element_by_xpath(
-                '//*[@id="serverSideDataTable"]/tbody/tr[' + str(i + 1) + ']/td[5]/span').text
+            car_make = table.find_element_by_xpath('.//tr[' + str(i + 1) + ']/td[5]/span').text
             list_of_car.append(car_make)
         self.assertTrue("PORSCHE" in list_of_car, "Porsche is not listed")
 
--- Challanges/challenge3/challenge3.py
+++ Challanges/challenge3/challenge3.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.by import By
 
 
 class Challenge3(unittest.TestCase):
-    """This challenge requires listing of most popular items"""
+    """This challenge requires listing of all teh makes/model under Most Popular Items."""
 
     def setUp(self) -> None:
         self.driver = webdriver.Chrome("../drivers/chromedriver")
@@ -15,37 +15,28 @@ class Challenge3(unittest.TestCase):
     def tearDown(self) -> None:
         self.driver.close()
 
-    def test_challenge1(self):
+    def test_challenge3git (self):
         # launch the site
         self.driver.get("https://www.copart.com")
         self.assertIn("Copart USA", self.driver.title)
 
         self.driver.maximize_window()
 
-
-
-
-
-
-        # exotic search page
-        self.driver.find_element_by_link_text("Exotics").click()
-        element = WebDriverWait(self.driver, 10).until(
+        # most popular table
+        most_popular_table = WebDriverWait(self.driver, 10).until(
             EC.presence_of_element_located(
-                (By.XPATH, '//*[@id="mainBody"]/div[1]/div/div[1]/div[1]/div/div[2]/div[3]/h1/span[2]')))
-        text = element.text
-        self.assertIn("exotic".lower(), str(text).lower())
+                (By.XPATH, '//*[@id="tabTrending"]/div[1]/div[2]')))
 
-        # validate Porsche is listed
-        list_of_car = list()
-        table = WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located(
-                (By.XPATH, '//*[@id="serverSideDataTable"]/tbody/tr')))
-        tableRows = self.driver.find_elements_by_xpath('//*[@id="serverSideDataTable"]/tbody/tr')
-        for i in range(len(tableRows)):
-            car_make = self.driver.find_element_by_xpath(
-                '//*[@id="serverSideDataTable"]/tbody/tr[' + str(i + 1) + ']/td[5]/span').text
-            list_of_car.append(car_make)
-        self.assertTrue("PORSCHE" in list_of_car, "Porsche is not listed")
+        list_columns = most_popular_table.find_elements_by_tag_name("div")
+
+        for i in range(len(list_columns)):
+            column = most_popular_table.find_element_by_xpath('.//div[' + str(i + 1) + ']/ul')
+            rows = column.find_elements_by_tag_name("li")
+            for j in range(len(rows)):
+                cell = column.find_element_by_xpath('.//li[' + str(j + 1) + ']/a')
+                make = cell.text
+                url = cell.get_attribute("href")
+                print(f"{make} - {url}")
 
 
 if __name__ == '__main__':
